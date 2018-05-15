ee15b6a26781502bde1dbcba01f8b879e6df9ad7
laess <laessig.pauline@aol.com>
2018-05-15 10:25:02 +0200

    1.0.1

--- src/ObserverListe.java
+++ src/ObserverListe.java
@@ -1,3 +1,15 @@
+import java.util.Observer;
+
 public class ObserverListe {
-    
+
+    public static void main (String[]args){
+        ObserverListe observer = new ObserverListe();
+    }
+
+    public ObserverListe(){
+
+    }
 }
+//Strg T update
+//Strg Shift K hochladen
+//Shift K committen
